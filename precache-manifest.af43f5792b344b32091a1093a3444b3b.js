f9ad08259c98689ff91a35306fa8cf0abea95b5c
Michael Hansen <michaelandhansen@gmail.com>
2020-11-22 14:33:13 -0500

    Updates

---
+++ precache-manifest.af43f5792b344b32091a1093a3444b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa631268b99fe5689766a4dc8cfc719",
+    "url": "/aphid-demo/index.html"
+  },
+  {
+    "revision": "27445e0ba026f80c0bc8",
+    "url": "/aphid-demo/static/css/main.dad1d7c5.chunk.css"
+  },
+  {
+    "revision": "1362a9c409b3140dad69",
+    "url": "/aphid-demo/static/js/2.cf617037.chunk.js"
+  },
+  {
+    "revision": "3a24589c4bb9995cbc50abf564c6ffde",
+    "url": "/aphid-demo/static/js/2.cf617037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27445e0ba026f80c0bc8",
+    "url": "/aphid-demo/static/js/main.6816f70e.chunk.js"
+  },
+  {
+    "revision": "5a9c02339c5e2be6bbaa",
+    "url": "/aphid-demo/static/js/runtime-main.ef0597ad.js"
+  }
+]);
